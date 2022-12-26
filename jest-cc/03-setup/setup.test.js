136a15a05b98674112bb9651a5f2790eaf951b3f
Irfan Dayan <engr.irfandayan@gmail.com>
2022-12-26 20:51:11 +0500

    add before each

---
+++ jest-cc/03-setup/setup.test.js
@@ -0,0 +1,35 @@
+let animals = ["elephant", "zebra", "bear", "tiger"];
+
+beforeAll(() => {
+  console.log("BEFORE ALL");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+
+beforeEach(() => {
+  console.log("BEFORE EACH");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+
+afterEach(() => {
+  console.log("AFTER EACH");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+
+afterAll(() => {
+  console.log("BEFORE ALL");
+  animals = ["elephant", "zebra", "bear", "tiger"];
+});
+describe("animals array", () => {
+  it("should add animals to end of array", () => {
+    animals.push("aligator");
+    expect(animals[animals.length - 1]).toBe("aligator");
+  });
+
+  it("should add animals to beginning of array", () => {
+    animals.unshift("monkey");
+    expect(animals[0]).toBe("monkey");
+  });
+  it("should have initial lenght of 4", () => {
+    expect(animals.length).toBe(4);
+  });
+});
